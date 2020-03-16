6bd76d0e606d8ab9e757067fd0636975a6f77143
Amanda Hogan <ahogan@teamworks.com>
2020-03-16 14:36:58 -0400

    Verify CRUD routes work

--- api/server.js
+++ api/server.js
@@ -6,12 +6,19 @@ const server = express();
 
 server.use(express.json());
 
-server.post('/api/accounts', (req, res) => {
-  // Validate that name is there / unique and that budget is there
-  // Insert req.body
-
-  db('accounts').insert()
-    .then()
+server.post('/api/accounts', validateAccount, validateUniqueName, (req, res) => {
+  db('accounts').insert(req.body)
+    .then(account => {
+      console.log('Successfully created the account');
+      db('accounts')
+        .then(accounts => {
+          res.status(200).json({ data: accounts });
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "The account information could not be retrieved" });
+        });
+    })
     .catch(error => {
       console.log(error);
       res.status(500).json({ message: "The account could not be created" });
@@ -21,7 +28,7 @@ server.post('/api/accounts', (req, res) => {
 server.get('/api/accounts', (req, res) => {
   db('accounts')
     .then(accounts => {
-      res.status(200).json(accounts);
+      res.status(200).json({ data: accounts });
     })
     .catch(error => {
       console.log(error);
@@ -35,9 +42,9 @@ server.get('/api/accounts/:id', (req, res) => {
   db('accounts').where({ id })
     .then(account => {
       if (account.length) {
-        res.status(200).json(account);
+        res.status(200).json({ data: account });
       } else {
-        res.status(400).json({ message: "Invalid account id" });
+        res.status(404).json({ message: "Invalid account id" });
       }
     })
     .catch(error => {
@@ -52,17 +59,17 @@ server.delete('/api/accounts/:id', (req, res) => {
   db('accounts').where({ id }).del()
     .then(count => {
       if (count > 0) {
+        console.log('Successfully deleted the account');
         db('accounts')
           .then(accounts => {
-            console.log('Successfully deleted the account');
-            res.status(200).json(accounts);
+            res.status(200).json({ data: accounts });
           })
           .catch(error => {
             console.log(error);
             res.status(500).json({ message: "The account information could not be retrieved" });
           });
       } else {
-        res.status(400).json({ message: "Invalid account id" });
+        res.status(404).json({ message: "Invalid account id" });
       }
     })
     .catch(error => {
@@ -71,18 +78,52 @@ server.delete('/api/accounts/:id', (req, res) => {
     });
 });
 
-server.put('/api/accounts/:id', (req, res) => {
-  // Validate that name is there / unique and that budget is there
-  // Update req.body
-  
+server.put('/api/accounts/:id', validateUniqueName, (req, res) => {
   const { id } = req.params;
 
-  db('accounts').where({ id }).update()
-    .then()
+  db('accounts').where({ id }).update(req.body)
+    .then(count => {
+      if (count > 0) {
+        console.log('Successfully updated the account');
+        db('accounts')
+          .then(accounts => {
+            res.status(200).json({ data: accounts });
+          })
+          .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "The account information could not be retrieved" });
+          });
+      } else {
+        res.status(404).json({ message: "Invalid account id" });
+      }
+    })
     .catch(error => {
       console.log(error);
       res.status(500).json({ message: "The account could not be updated" });
     });
 });
+
+function validateAccount(req, res, next) {
+  if (!req.body.name || !req.body.budget) {
+    res.status(400).json({ message: "Account must have a name and a budget to be created" })
+  } else {
+    next();
+  };
+};
+
+function validateUniqueName(req, res, next) {
+  db('accounts')
+    .then(accounts => {
+      const accountNames = accounts.filter(account => { return account.name === req.body.name });
+      if (accountNames.length) {
+        res.status(400).json({ message: "Name already exists, please choose a unique value" });
+      } else {
+        next();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 
 module.exports = server;
--- queries.sql
+++ queries.sql
@@ -13,13 +13,13 @@ WHERE SupplierID = '11';
 -- List first 10 orders placed, sorted descending by the order date
 
 SELECT * FROM Orders 
-ORDER BY OrderDate desc 
+ORDER BY OrderDate DESC 
 LIMIT 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
 
 SELECT * FROM Customers 
-WHERE City = 'London' or City = 'Madrid' or Country = 'Brazil';
+WHERE City = 'London' OR City = 'Madrid' OR Country = 'Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
